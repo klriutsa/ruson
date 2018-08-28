b0d72c4107593db21d80d8f4704fb210fc36d0e8
klriutsa <hiroya.kurushima@litalico.co.jp>
2018-08-28 13:31:03 +0900

    :bug: fix nested ruson class convert

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Ruson::Base do
     field :test_name, name: 'source_key_name'
   end
 
+  class RusonBaseTestSubClass3 < Ruson::Base
+    field :object, class: RusonBaseTestSubClass
+    field :objects, each_class: RusonBaseTestSubClass2
+  end
+
+  let(:text){ '{"object": { "test_name": "object_name" }, "objects": [ { "source_key_name": "1" }, { "source_key_name": "2" }, { "source_key_name": "3" } ] }' }
+
   it do
     obj = RusonBaseTestSubClass.new({})
     expect(obj).to respond_to :test_name
@@ -45,5 +52,17 @@ RSpec.describe Ruson::Base do
   it do
     obj = RusonBaseTestSubClass2.new({ source_key_name: 'test' }.to_json)
     expect(obj.test_name).to eq 'test'
+  end
+
+  it do
+    obj = RusonBaseTestSubClass3.new(text)
+    expect(obj.object.class).to eq RusonBaseTestSubClass
+    expect(obj.object.test_name).to eq 'object_name'
+
+    expect(obj.objects.first.class).to eq RusonBaseTestSubClass2
+    expect(obj.objects.size).to eq 3
+    expect(obj.objects[0].test_name).to eq '1'
+    expect(obj.objects[1].test_name).to eq '2'
+    expect(obj.objects[2].test_name).to eq '3'
   end
 end
