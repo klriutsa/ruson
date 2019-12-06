11a7f4841922cecd1e6565c7e6b331351c767139
Guillaume Hain <zedtux@zedroot.org>
2019-12-06 06:14:02 +0100

    Adds Array primary class

--- lib/ruson/json.rb
+++ lib/ruson/json.rb
@@ -1,7 +1,9 @@
 module Ruson
   module Json
     def get_hash_from_json(json)
+      return unless json
       return json if json.class == ActiveSupport::HashWithIndifferentAccess
+
       (json.class == Hash ? json : JSON.parse(json)).with_indifferent_access
     end
   end
