0d7164d7d2ae98f9b98d592eeaab656392325a24
Guillaume Hain <zedtux@zedroot.org>
2019-12-06 04:59:55 +0100

    Implements the `first` and `first!` querying methods (Relates #22)

--- lib/ruson/querying.rb
+++ lib/ruson/querying.rb
@@ -12,10 +12,7 @@ module Ruson
 
         return unless File.exist?(file_path)
 
-        json = JSON.parse(File.read(file_path))
-        json[:id] = id
-
-        new json
+        load(file_path, id: id)
       end
 
       def find!(id)
@@ -24,6 +21,34 @@ module Ruson
         raise Ruson::RecordNotFound unless record
 
         record
+      end
+
+      def first
+        ensure_output_folder_is_defined
+
+        file_path = Dir.glob(File.join(model_base_path, '*.json')).first
+
+        return unless file_path
+
+        id = File.basename(file_path, '.json').to_i
+
+        load(file_path, id: id)
+      end
+
+      def first!
+        record = first
+
+        raise Ruson::RecordNotFound unless record
+
+        record
+      end
+
+      def load(file_path, extra_json = {})
+        json = JSON.parse(File.read(file_path))
+
+        json.merge!(extra_json) if extra_json
+
+        new json
       end
     end
   end
